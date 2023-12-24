b945c3039cf0b2aa6fe8cc8dad69670878f19323
Sculas <contact@sculas.xyz>
2023-12-24 19:18:25 +0100

    fix: fix event name issue in config

--- Config/EventConfig.cs
+++ Config/EventConfig.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using BepInEx;
 using BrutalCompanyPlus.Api;
+using BrutalCompanyPlus.Utils;
 
 namespace BrutalCompanyPlus.Config;
 
@@ -11,10 +13,25 @@ internal static class EventConfig {
     internal static void RegisterWith(BaseUnityPlugin FromPlugin, ref List<IEvent> Events) {
         // Apply user configuration
         foreach (var @event in Events) {
-            // Apply event rarity
-            EventRegistry.EventRarityValues[@event] = FromPlugin.Config.Bind(
-                Category, @event.Name, @event.DefaultRarity, @event.Description
-            ).Value;
+            try {
+                // Sanitize event name (illegal characters in config keys)
+                var eventName = @event.Name.Replace("'", "");
+                // Apply event rarity
+                EventRegistry.EventRarityValues[@event] = FromPlugin.Config.Bind(
+                    Category, eventName, @event.DefaultRarity, @event.Description
+                ).Value;
+            } catch (Exception e) {
+                // Log the error
+                Plugin.Logger.LogError(e.ToString());
+                var from = FromPlugin.Info.Metadata.GUID != PluginInfo.PLUGIN_GUID
+                    ? $" from {FromPlugin.Info.Metadata.Name}"
+                    : "";
+                Diagnostics.AddError(
+                    $"Failed to register event \"{@event.Name}\"{from}! " +
+                    $"See console for details.");
+                // Apply default rarity as a fallback and continue
+                EventRegistry.EventRarityValues[@event] = @event.DefaultRarity;
+            }
         }
     }
 }
