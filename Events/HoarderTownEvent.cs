4b1620b9ae882b8c89a84707cd3ec9ebff60de38
Sculas <contact@sculas.xyz>
2023-12-24 18:30:33 +0100

    feat: Add `Hoarder Town` event

---
+++ Events/HoarderTownEvent.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable InconsistentNaming
+
+using BrutalCompanyPlus.Api;
+using BrutalCompanyPlus.Objects;
+using JetBrains.Annotations;
+
+namespace BrutalCompanyPlus.Events;
+
+[UsedImplicitly]
+public class HoarderTownEvent : IEvent {
+    public string Name => "Hoarder Town";
+    public string Description => "Come on, those are mine!";
+    public EventPositivity Positivity => EventPositivity.Negative;
+    public EventRarity DefaultRarity => EventRarity.Rare;
+
+    public void ExecuteServer(SelectableLevel Level) {
+        EnemySpawnManager.DraftEnemySpawn<HoarderBugAI>(new EnemySpawnManager.SpawnInfo(8));
+        EnemySpawnManager.DraftEnemySpawn<HoarderBugAI>(new EnemySpawnManager.SpawnInfo(8, Outside: true));
+    }
+
+    public void ExecuteClient(SelectableLevel Level) { }
+}
