3360d0c896e147fa40acb939e1f916f220598a84
Sculas <contact@sculas.xyz>
2023-12-24 02:38:05 +0100

    feat: Add `Hide and Seek` event
    Formerly called "They just wants to play!!"

---
+++ Events/HideAndSeekEvent.cs
@@ -0,0 +1,21 @@
+﻿// ReSharper disable InconsistentNaming
+
+using BrutalCompanyPlus.Api;
+using BrutalCompanyPlus.Objects;
+using JetBrains.Annotations;
+
+namespace BrutalCompanyPlus.Events;
+
+[UsedImplicitly]
+public class HideAndSeekEvent : IEvent {
+    public string Name => "Hide and Seek";
+    public string Description => "She's gonna find you...";
+    public EventPositivity Positivity => EventPositivity.Negative;
+    public EventRarity DefaultRarity => EventRarity.Uncommon;
+
+    public void ExecuteServer(SelectableLevel Level) {
+        EnemySpawnManager.DraftEnemySpawn<DressGirlAI>(new EnemySpawnManager.SpawnInfo(5));
+    }
+
+    public void ExecuteClient(SelectableLevel Level) { }
+}
