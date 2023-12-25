d7109f2b76d80e470c910d7cc784f15bee972551
Sculas <contact@sculas.xyz>
2023-12-25 19:57:17 +0100

    refactor(SpringbreakEvent): Nerf amount of coil-heads that spawn
    This is one hell of an event, lol. Made the event a bit more rare as well.

--- Events/SpringbreakEvent.cs
+++ Events/SpringbreakEvent.cs
@@ -15,10 +15,10 @@ public class SpringbreakEvent : IEvent {
     public string Name => "Springbreak";
     public string Description => "Don't get a concussion!";
     public EventPositivity Positivity => EventPositivity.Negative;
-    public EventRarity DefaultRarity => EventRarity.Uncommon;
+    public EventRarity DefaultRarity => EventRarity.Rare;
 
     public void ExecuteServer(SelectableLevel Level) {
-        EnemySpawnManager.DraftEnemySpawn<SpringManAI>(new EnemySpawnManager.SpawnInfo(4, Outside: true));
+        EnemySpawnManager.DraftEnemySpawn<SpringManAI>(new EnemySpawnManager.SpawnInfo(3, Outside: true));
     }
 
     public void ExecuteClient(SelectableLevel Level) { }
