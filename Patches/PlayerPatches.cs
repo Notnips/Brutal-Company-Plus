cee5a9381e0a797fde3bf7116235893d1d0d7600
Sculas <contact@sculas.xyz>
2023-12-26 22:40:01 +0100

    fix: Fix footstep audio cutting out
    Adds a patch that sets the player movement audio priority to be higher than the enemy SFX audio. This fixes cases where there are too many enemies producing sound, cutting out footsteps.

---
+++ Patches/PlayerPatches.cs
@@ -0,0 +1,16 @@
+﻿// ReSharper disable InconsistentNaming,RedundantAssignment
+
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace BrutalCompanyPlus.Patches;
+
+[HarmonyPatch(typeof(PlayerControllerB))]
+internal static class PlayerPatches {
+    [HarmonyPostfix, HarmonyPatch("Start")]
+    private static void ShowDiagnosticsPatch(PlayerControllerB __instance) {
+        // Originally, this is set to 129, while the enemy SFX is set to 128.
+        // This causes the enemy SFX to be played over the player SFX because lower numbers have higher priority.
+        __instance.movementAudio.priority = 127;
+    }
+}
