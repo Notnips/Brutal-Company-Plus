30df16093caa4e0754a55fd8f50a19abcd5ff183
Sculas <contact@sculas.xyz>
2023-12-27 00:32:10 +0100

    refactor(UnfairCompanyEvent): Spawn 3 extra enemies outside

--- Events/UnfairCompanyEvent.cs
+++ Events/UnfairCompanyEvent.cs
@@ -1,5 +1,6 @@
 ﻿using BrutalCompanyPlus.Api;
 using BrutalCompanyPlus.Objects;
+using BrutalCompanyPlus.Utils;
 using JetBrains.Annotations;
 using UnityEngine;
 
@@ -7,12 +8,19 @@ namespace BrutalCompanyPlus.Events;
 
 [UsedImplicitly]
 public class UnfairCompanyEvent : IEvent {
+    private const int OutsideEnemyCount = 3;
+
     public string Name => "Unfair Company";
     public string Description => "Sometimes, life just ain't fair.";
     public EventPositivity Positivity => EventPositivity.Negative;
     public EventRarity DefaultRarity => EventRarity.VeryRare;
 
     public void ExecuteServer(SelectableLevel Level) {
+        for (var i = 0; i < OutsideEnemyCount; i++) {
+            if (!LevelManager.AllOutsideEnemies.Random(out var type)) continue;
+            EnemySpawnManager.DraftEnemySpawn(type, new EnemySpawnManager.SpawnInfo(1, Outside: true));
+        }
+
         // ReSharper disable once VariableHidesOuterVariable
         LevelManager.ModifyLevelProperties(Level, Level => {
                 Level.outsideEnemySpawnChanceThroughDay = new AnimationCurve(
