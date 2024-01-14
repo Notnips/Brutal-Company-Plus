74787bf5223be7031699977886ac24d7a1ce0c20
Sculas <contact@sculas.xyz>
2024-01-14 23:48:10 +0100

    feat(ChatUtils): Add SendLocal API

--- Utils/ChatUtils.cs
+++ Utils/ChatUtils.cs
@@ -1,4 +1,6 @@
-﻿namespace BrutalCompanyPlus.Utils;
+﻿// ReSharper disable MemberCanBePrivate.Global
+
+namespace BrutalCompanyPlus.Utils;
 
 public static class ChatUtils {
     public static void Send(string Message, bool Clear = false) {
@@ -6,7 +8,15 @@ public static class ChatUtils {
         HUDManager.Instance.AddTextToChatOnServer(Message);
     }
 
+    public static void SendLocal(string Message, bool Clear = false) {
+        if (Clear) ChatUtils.Clear();
+        HUDManager.Instance.AddChatMessage(Message);
+    }
+
     private static void Clear() => Send(
         "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
     );
+
+    internal static void NotifyError() =>
+        SendLocal($"An error occurred in {PluginInfo.PLUGIN_NAME}. Check the console for more information.");
 }
